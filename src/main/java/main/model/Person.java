8b22d64ecbdab4d6a512e1c088b4d5bc513fd8e2
empope1 <empope1@dmacc.edu>
2024-04-03 16:42:54 -0500

    Emily Class

---
+++ src/main/java/main/model/Person.java
@@ -0,0 +1,38 @@
+package main.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author emilylester empope1
+ * CIS 175 Spring 2024
+ * Apr 3, 2024
+ */
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+	
+	//Attributes
+	@Id
+	@GeneratedValue
+	private int guestId;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthday;
+	private String email;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private int phoneNo;
+
+}
