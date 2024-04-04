534ef0ee8d05c6f4a15020c71b916698faf65b9f
mayacruzz <138790217+mayacruzz@users.noreply.github.com>
2024-04-04 03:27:54 -0500

    Commit

---
+++ JavaFinalVacationBookingApp-main/src/main/java/main/JavaIiFinalTravelBookingAppApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIiFinalTravelBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIiFinalTravelBookingAppApplication.class, args);
+	}
+
+}
