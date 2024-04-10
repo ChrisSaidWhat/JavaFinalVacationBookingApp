c2d919cf51ec90abc783dc4435923c57c4d07b92
Christopher Said <christopher.w.said@gmail.com>
2024-04-10 17:52:18 -0500

    cleaned up Trip.java

--- src/main/java/main/model/Trip.java
+++ src/main/java/main/model/Trip.java
@@ -1,6 +1,7 @@
 package main.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.print.attribute.standard.Destination;
 
@@ -25,21 +26,13 @@ public class Trip {
 	@GeneratedValue
 	private Long tripId;
 	private String title;
-<<<<<<< HEAD
-	private List <Destination> destination;
 	private List <Person> person;
-	private List <CarRental> carRental;
-	private List <Lodging> lodging;
-	private List <Flights> flights;
-=======
 	private Destination destination;
-	private Person person;
 	private LocalDate leaveDate;
 	private LocalDate returnDate;
 	private CarRental carRental;
 	private Lodging lodging;
 	private Flights flights;
->>>>>>> 46d122cf7df47c781453d527b0723de6d9d664d2
 	private int numGuests;
 	private double totalPrice;
 	
