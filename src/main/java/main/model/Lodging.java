2a73f8874bdeb97d4d5c8dfa7a6347f0a5e82e9f
mistichris <codeine500@hotmail.com>
2024-04-10 11:03:49 -0500

    added the lodging class to start filling in some gaps, filling in parts of the webcontroller just to get started

--- src/main/java/main/model/Lodging.java
+++ src/main/java/main/model/Lodging.java
@@ -2,8 +2,6 @@ package main.model;
 
 import java.time.LocalDate;
 
-import javax.print.attribute.standard.Destination;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,26 +12,24 @@ import lombok.NoArgsConstructor;
 /**
  * @author Misti Christianson - mchristianson
  * CIS175 - Spring 2024
- * Apr 3, 2024
+ * Apr 10, 2024
  */
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Trip {
+public class Lodging {
 	@Id
 	@GeneratedValue
-	private Long tripId;
-	private String title;
-	private Destination destination;
-	private Person person;
-	private LocalDate leaveDate;
-	private LocalDate returnDate;
-	private CarRental carRental;
-	private Lodging lodging;
-	private Flights flights;
-	private int numGuests;
-	private double totalPrice;
-	
-	
+	private int lodgingId;
+	private String lodgingName;
+	private LocalDate checkInDate;
+	private LocalDate checkOutDate;
+	private String lodgingType;
+	private int maxCapacity;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private double rate;
 }
